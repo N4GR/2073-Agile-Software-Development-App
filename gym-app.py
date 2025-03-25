bf4ca8aa3bb2aae3a65d376a1564842a3d94621e
Morgan <107074986+N4GR@users.noreply.github.com>
2025-03-25 09:05:11 +0000

    Clean Slate

--- gym-app.py
+++
@@ -1,8 +0,0 @@
-from src.windows.main_window import MainWindow
-from src.application.application import Application
-
-if __name__ == "__main__":
-    application = Application([])
-    main_window = MainWindow()
-    
-    application.exec()
