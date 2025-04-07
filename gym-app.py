7d2b023ae9cce703522f010a59ed7d3b12501a97
Morgan <107074986+N4GR@users.noreply.github.com>
2025-04-07 07:15:13 +0100

    UPDATE | Startup script

--- gym-app.py
+++ gym-app.py
@@ -1,7 +1,17 @@
+import os
+
+from src.shared.startup import startup
+
 from src.application.application import Application
 from src.windows.main_window import MainWindow
 
 if __name__ == "__main__":
+    startup_error = startup()
+    
+    if startup_error is True:
+        print("Startup error occoured, exitting launch.")
+        os._exit() # Exit early.
+    
     # If this is the file being ran, run the application and the main window.
     application = Application() # Load the application object.
     main_window = MainWindow() # Load the main window object.
---
+++ src/shared/startup.py
@@ -0,0 +1,42 @@
+# File to run as the program starts up.
+import os
+import sqlite3
+
+from src.shared.funcs import path
+
+def startup() -> bool:
+    required_databases = ["chat.sqlite", "gym.sqlite"]
+    database_list = os.listdir(path("/data"))
+    
+    for file in database_list:
+        if file in required_databases:
+            required_databases.remove(file)
+    
+    # If there's any still left in the required, create the database.
+    for database in required_databases:
+        database_name = database.replace(".sqlite", "")
+        schema_path = path(f"/data/schemas/{database_name}.sql")
+        
+        print(f"Generating {database} from /data/schemas/{database_name}.sql")
+        
+        with open(schema_path, "r") as file:
+            sql_query = file.read()
+        
+        # Create the database with a cursor.
+        connection = sqlite3.connect(path(f"/data/{database}"))
+        cursor = connection.cursor()
+        
+        # Try and execute the query from the schema to the database.
+        try:
+            cursor.execute(sql_query)
+            connection.commit()
+        
+        except sqlite3.Error as error:
+            print(f"Error creating database: {error}")
+            
+            return True # Error occoured, exit starting program.
+        
+        finally:
+            connection.close()
+    
+    return False
